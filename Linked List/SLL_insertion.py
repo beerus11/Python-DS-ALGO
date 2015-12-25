40d4cf1b827850e3da5755b0c24cc5facf4f7ebb
Anurag Goel <thechampanurag@gmail.com>
2015-12-25 10:45:05 +0530

    Singly Linked List Insertion

---
+++ Linked List/SLL_insertion.py
@@ -0,0 +1,39 @@
+__author__ = 'Anurag Goel'
+class Node:
+    def init(self):
+        self.data=None
+        self.next=None
+    def setData(self,data):
+        self.data=data
+    def getData(self):
+        return self.data
+    def setNext(self,next):
+        self.next=next
+    def getNext(self):
+        return self.next
+    def hasNext(self):
+        return self.next!=None
+
+print "Enter No of Elements to Insert : "
+num=int(input())
+length=num
+head=None
+tail=None
+while(num!=0):
+    if length==num:
+        head=Node()
+        head.setData(int(input("Enter a No to Insert : ")))
+        tail=Node()
+        head.setNext(tail)
+    else:
+        tail.setData(int(input("Enter a No to Insert : ")))
+        node=Node()
+        tail.setNext(node)
+        tail=node
+    num=num-1
+    
+print "Elements In Linked List After Insertion : "
+while(length):
+    print head.getData()
+    head=head.getNext()
+    length=length-1
