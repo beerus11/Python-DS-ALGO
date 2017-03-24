e09186c9c4a2e5ecb60c6a8e4c10dab8633c0cec
Anurag Goel <thechampanurag@gmail.com>
2017-03-24 08:14:04 +0530

    Array, DP, String

--- DP/knapsack.py
+++ DP/knapsack.py
@@ -1,19 +1,17 @@
-def knapsack(W,wt,val,n):
-    k=[[0 for z in range(W+1)]for x in range(0,n+1)]
-        # Build table K[][] in bottom up manner
+def knapSack(W, wt, val, n):
+    k= [[0 for x in range(W+1)] for y in range(n+1)]
     for i in range(n+1):
-        for w in range(W+1):
-            if i==0 or w==0:
-                K[i][w] = 0
-            elif wt[i-1] <= w:
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
+        for j in range(W+1):
+            if i==0 or j==0:
+                k[i][j]=0
+            elif wt[i-1]<=j:
+                k[i][j]=max(k[i-1][j],val[i-1]+k[i-1][j-wt[i-1]])
             else:
-                K[i][w] = K[i-1][w]
+                k[i][j]=k[i-1][j]
+    return k[n][W]
 
-    return K[n][W]
-
-val=[60,100,120]
-wt=[10,20,30]
-W=50
-n=len(val)
-print knapsack(W,wt,val,n)
+val = [60, 100, 120]
+wt = [10, 20, 30]
+W = 50
+n = len(val)
+print(knapSack(W, wt, val, n))
